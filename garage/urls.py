3a57258c0327f50db1f4966985ac72f061a4da0b
BaaAza <bagdadazahaf@gmail.com>
2018-07-13 11:38:56 +0200

    ajout Html Liste et creation client, ajout ordre_reparation.html, urls et views

--- garage/urls.py
+++ garage/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'garage'
 
 urlpatterns = [
-    
-
+    path('client', views.client, name='client'),    
+    path('reparation/<int:client_id>/', views.ordre_reparation, name='reparation'),    
 ]
--- garage/views.py
+++ garage/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Client
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ClientForm
+from django.views.generic.list import ListView
+
+
+# class ListeClients(ListView):
+#     model = Client
+#     context_object_name = "derniers_clients"
+#     template_name = "garage/client_list.html"
+
+
+def ordre_reparation(request, client_id):
+    client = Client.objects.get(pk=client_id)
+    context = {
+        'client': client
+    }
+    # client = get_object_or_404(Client, id=id)
+    return render(request, 'garage/reparation.html', context)    
+
+
+def client(request):
+    sauvegarde = False
+    form = ClientForm(request.POST or None)    
+    if form.is_valid():
+        client = Client()
+        client.nom_client = form.cleaned_data["nom_client"]
+        client.prenom_client = form.cleaned_data["prenom_client"]
+        client.telephone_client = form.cleaned_data["telephone_client"]        
+        client.email_client = form.cleaned_data["email_client"]    
+        client.adresse_client = form.cleaned_data["adresse_client"]
+        client.code_postal_client = form.cleaned_data["code_postal_client"]
+        client.ville_client = form.cleaned_data["ville_client"]
+        client.pays_client = form.cleaned_data["pays_client"]
+        client.save()
+        sauvegarde = True
+        return redirect("garage:reparation", client_id=client.id)
+
+    return render(
+        request, 
+        'garage/list_creer_client.html', 
+        {   'form': form,
+            'sauvegarde' : sauvegarde,
+            'context_object_name': Client.objects.all()}
+    )
+
+
+
+
