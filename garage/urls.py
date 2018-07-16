f987a3cae02ea6812165a84eb79b32b626ffa9ca
BaaAza <bagdadazahaf@gmail.com>
2018-07-16 10:01:06 +0200

    ajout views, urls, forms, templates de la création des clients, liste des clients, recherche client et ordre de réparation 1er version non finalisé

--- garage/urls.py
+++ garage/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+
+from django.views.generic import ListView
 from . import views
 
+from .models import Client
+from django.conf.urls import url
 
 app_name = 'garage'
 
 urlpatterns = [
     path('client', views.client, name='client'),    
-    path('reparation/<int:client_id>/', views.ordre_reparation, name='reparation'),    
+    # path('client/<int:client_id>/', views.modifier_client, name='modifier'),    
+    path('reparation/<int:client_id>/', views.ordre_reparation, name='reparation'),
+    url(r'^recherche/$', views.recherche, name='recherche'),
+    # url(r'^search/$', views.recherche, name='recherche'),
 ]
--- garage/views.py
+++ garage/views.py
@@ -18,7 +18,7 @@ def ordre_reparation(request, client_id):
         'client': client
     }
     # client = get_object_or_404(Client, id=id)
-    return render(request, 'garage/reparation.html', context)    
+    return render(request, 'garage/ordre_reparation.html', context)    
 
 
 def client(request):
@@ -28,15 +28,15 @@ def client(request):
         client = Client()
         client.nom_client = form.cleaned_data["nom_client"]
         client.prenom_client = form.cleaned_data["prenom_client"]
-        client.telephone_client = form.cleaned_data["telephone_client"]        
-        client.email_client = form.cleaned_data["email_client"]    
-        client.adresse_client = form.cleaned_data["adresse_client"]
-        client.code_postal_client = form.cleaned_data["code_postal_client"]
-        client.ville_client = form.cleaned_data["ville_client"]
-        client.pays_client = form.cleaned_data["pays_client"]
+        # client.telephone_client = form.cleaned_data["telephone_client"]        
+        # client.email_client = form.cleaned_data["email_client"]    
+        # client.adresse_client = form.cleaned_data["adresse_client"]
+        # client.code_postal_client = form.cleaned_data["code_postal_client"]
+        # client.ville_client = form.cleaned_data["ville_client"]
+        # client.pays_client = form.cleaned_data["pays_client"]
         client.save()
         sauvegarde = True
-        return redirect("garage:reparation", client_id=client.id)
+        return redirect("garage:ordre_reparation", client_id=client.id)
 
     return render(
         request, 
@@ -45,6 +45,20 @@ def client(request):
             'sauvegarde' : sauvegarde,
             'context_object_name': Client.objects.all()}
     )
+
+
+def recherche(request):
+    query = request.GET.get('query')
+    if not query:
+        clients = Client.objects.all()
+    else:
+        # nom_client contains the query is and query is not sensitive to case.
+        clients = Client.objects.filter(nom_client__icontains=query)
+    title = "Résultats pour la requête %s"%query
+    context = {
+        'context_object_name': clients
+    }
+    return render(request, 'garage/recherche.html', context)  
 
 
 
