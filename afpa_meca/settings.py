b1647b388285dfbdb0c75a3196e48f36abe8dab4
Jeffnad <jeff11.nad@gmail.com>
2018-07-26 15:32:36 +0200

    regler les conflits

--- afpa_meca/settings.py
+++ afpa_meca/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'afpa_meca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-# DATABASES = DATABASES_DEV
+DATABASES = DATABASES_DEV
 
 
 # Password validation
