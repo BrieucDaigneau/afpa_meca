59930c27d985e023a94caeb533fcfd63dda45dde
Jeffnad <jeff11.nad@gmail.com>
2018-07-26 14:46:15 +0200

    mise ajour login required

--- garage/urls.py
+++ garage/urls.py
@@ -5,6 +5,8 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Client
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
+
 
 app_name = 'garage'
 
@@ -12,7 +14,7 @@ urlpatterns = [
     # path('client-create', views.ClientDonneePersoCreate.as_view(), name='client-create'),    
     # path('client-create', views.ClientCreate.as_view(), name='client-create'),    
     # path('client', views.client, name='client'),   
-    path('client-create', views.clientCreate, name='client-create'),   
+    path('client-create', login_required(views.clientCreate), name='client-create'),   
     # path('client/<int:client_id>/', views.modifier_client, name='modifier'),    
     path('reparation/<int:client_id>/', views.ordre_reparation, name='ordre_reparation'),
     url(r'^recherche/$', views.recherche, name='recherche'),
@@ -20,6 +22,6 @@ urlpatterns = [
     url(r'login', LoginView.as_view(redirect_authenticated_user=True, template_name="garage/login.html"),
         name='login'),
     url(r'logout', LogoutView.as_view(template_name="garage/logout.html"), name='logout'),
-    path('accueil/', views.accueil, name='accueil'),
+    path('accueil/', login_required(views.accueil), name='accueil'),
     # path('garage/login/', include('django.contrib.auth.urls'), name="login"),
 ]
